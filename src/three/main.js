0b1184b24cd3737ed87ca10b11c0aa33a8429b03
Isaac Song <isaacsongs10@gmail.com>
2024-01-19 14:00:46 -0800

    set up three scene

--- src/three/main.js
+++ src/three/main.js
@@ -1,2 +1,19 @@
 //! Main File for three.js
 // Could be separated into different files and folders
+
+function main() {
+
+	const canvas = document.querySelector( '#c' );
+	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+
+	const fov = 40;
+	const aspect = 2; // the canvas default
+	const near = 0.1;
+	const far = 1000;
+	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+	camera.position.z = 120;
+
+	const scene = new THREE.Scene();
+}
+
+main();

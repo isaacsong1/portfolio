03fff9c50735ad178812056367753cc86bc1d515
Isaac Song <isaacsongs10@gmail.com>
2024-01-22 14:16:01 -0800

    start microsoft links

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-// Notable website designs:
+// Notable website designs (from cofolios.com):
 // https://www.zixianjia.com/ (Simple, nice dark color, Good spacing and font)
-// Google
+// Google links
 // https://ndrewgood.com/ (minimalistic, like the random pastel colors)
 // https://verlichen.com/ (scrolling, nice gray background)
 // https://www.cedricith.com/about (scrolling, nice about page, animations)
 // https://rocky.framer.website/ (apple-like design, nice cards and styling)
-
+// Microsoft links
+// https://www.yutongzhang.me/ (apple avatar)
 
 
 // Three js: Need to test local server -> npx vite

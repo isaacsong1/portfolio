a248a04125efd5097bc1f2003ef2473f1e736b17
Isaac Song <isaacsongs10@gmail.com>
2024-01-22 14:14:48 -0800

    finish google websites

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 // Notable website designs:
 // https://www.zixianjia.com/ (Simple, nice dark color, Good spacing and font)
+// Google
 // https://ndrewgood.com/ (minimalistic, like the random pastel colors)
 // https://verlichen.com/ (scrolling, nice gray background)
 // https://www.cedricith.com/about (scrolling, nice about page, animations)
+// https://rocky.framer.website/ (apple-like design, nice cards and styling)
+
 
 
 // Three js: Need to test local server -> npx vite

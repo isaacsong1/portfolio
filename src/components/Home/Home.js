93501df3773ededd3229ca494db2d9073d44ecfe
Isaac Song <isaacsongs10@gmail.com>
2024-01-22 14:11:58 -0800

    add two links

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 // Notable website designs:
 // https://www.zixianjia.com/ (Simple, nice dark color, Good spacing and font)
-
+// https://ndrewgood.com/ (minimalistic, like the random pastel colors)
+// https://verlichen.com/ (scrolling, nice gray background)
 
 // Three js: Need to test local server -> npx vite
 // next session: create jsx components

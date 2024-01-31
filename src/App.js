896dd0e8446f6f5b859d20651e050bd2068f6d64
Isaac Song <isaacsongs10@gmail.com>
2024-01-30 16:47:25 -0800

    canvas and render code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { BoxGeometry, Mesh, MeshStandardMaterial, PerspectiveCamera, Scene, WebGLRenderer }from 'three';
 
 function App() {
+  // https://www.youtube.com/watch?v=DPl34H2ISsk TUTORIAL
+  // Create Canvas
+  const scene = new Scene();
+  const camera = new PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+
+  // Render
+  const renderer = new WebGLRenderer({ alpha: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  const container = document.getElementById('root');
+
+  if (container) {
+    container.appendChild(renderer.domElement);
+  }
+
+  function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }
+
+  animate();
+
   return (
     <div className="App">
       <header className="App-header">
